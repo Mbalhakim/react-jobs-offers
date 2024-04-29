c4d46eac7abea62e5802fd9757a48e406021b802
Mbalhakim <mb.alhakim@gmail.com>
2024-04-29 19:34:25 +0200

    json-server added to genrate routes with the json file objects instead of importing the json file, job listing skelton made adn added on data loading

---
+++ src/Skeletons/JobListingSkeleton.jsx
@@ -0,0 +1,69 @@
+
+const JobListingSkeleton = () => {
+  return (
+    <>
+    <div className="bg-white rounded-xl shadow-md relative animate-pulse">
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Job type skeleton */}
+          <div className="w-40 h-6 bg-gray-200 mb-2"></div> {/* Job title skeleton */}
+        </div>
+        <div className="mb-5">
+          <div className="w-full h-4 bg-gray-200 mb-2"></div> {/* Description skeleton */}
+          <div className="w-3/4 h-4 bg-gray-200"></div>
+        </div>
+        <button className="w-20 h-6 bg-gray-200 mb-5"></button> {/* Toggle button skeleton */}
+        <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Salary skeleton */}
+        <div className="border border-gray-100 mb-5"></div>
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Location skeleton */}
+          <div className="w-20 h-4 bg-gray-200"></div>
+        </div>
+      </div>
+    </div>
+
+    <div className="bg-white rounded-xl shadow-md relative animate-pulse">
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Job type skeleton */}
+          <div className="w-40 h-6 bg-gray-200 mb-2"></div> {/* Job title skeleton */}
+        </div>
+        <div className="mb-5">
+          <div className="w-full h-4 bg-gray-200 mb-2"></div> {/* Description skeleton */}
+          <div className="w-3/4 h-4 bg-gray-200"></div>
+        </div>
+        <button className="w-20 h-6 bg-gray-200 mb-5"></button> {/* Toggle button skeleton */}
+        <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Salary skeleton */}
+        <div className="border border-gray-100 mb-5"></div>
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Location skeleton */}
+          <div className="w-20 h-4 bg-gray-200"></div>
+        </div>
+      </div>
+    </div>
+
+    <div className="bg-white rounded-xl shadow-md relative animate-pulse">
+      <div className="p-4">
+        <div className="mb-6">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Job type skeleton */}
+          <div className="w-40 h-6 bg-gray-200 mb-2"></div> {/* Job title skeleton */}
+        </div>
+        <div className="mb-5">
+          <div className="w-full h-4 bg-gray-200 mb-2"></div> {/* Description skeleton */}
+          <div className="w-3/4 h-4 bg-gray-200"></div>
+        </div>
+        <button className="w-20 h-6 bg-gray-200 mb-5"></button> {/* Toggle button skeleton */}
+        <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Salary skeleton */}
+        <div className="border border-gray-100 mb-5"></div>
+        <div className="flex flex-col lg:flex-row justify-between mb-4">
+          <div className="w-20 h-4 bg-gray-200 mb-2"></div> {/* Location skeleton */}
+          <div className="w-20 h-4 bg-gray-200"></div>
+        </div>
+      </div>
+    </div>
+
+    </>
+  );
+};
+
+export default JobListingSkeleton;
